980199f5bda8e2c465fa157c7541b573f37d11ea
Nathan Dane <nathan.dane@pushdr.com>
2019-03-05 10:35:15 +0000

    Support for Azure event: git.pullrequest.created

--- service/hook/visualstudioteamservices/visualstudioteamservices_test.go
+++ service/hook/visualstudioteamservices/visualstudioteamservices_test.go
@@ -19,6 +19,14 @@ const (
 		"publisherId": "tfs"
 	}`
 
+	samplePullRequestEmptySubscriptionID = `{
+		"subscriptionId": "00000000-0000-0000-0000-000000000000",
+		"notificationId": 4,
+		"id": "daae438c-296b-4512-b08e-571910874e9b",
+		"eventType": "git.pullrequest.created",
+		"publisherId": "tfs"
+	}`
+
 	sampleCodeGitPushBadEventType = `{
 		"subscriptionId": "f0c23515-bcd1-4e30-9613-56a0a129c732",
 		"notificationId": 4,
@@ -161,6 +169,264 @@ const (
 	    "url": "https://fabrikam-fiber-inc.visualstudio.com/DefaultCollection/_apis/git/repositories/278d5cd2-584d-4b63-824a-2ba458937249/pushes/14"
 	  },
 	  "createdDate": "2016-02-17T21:29:54.5474864Z"
+	}`
+
+	sampleCodeGitPullRequestCreated = `{
+		"id": "2ab4e3d3-b7a6-425e-92b1-5a9982c1269e",
+		"eventType": "git.pullrequest.created",
+		"publisherId": "tfs",
+		"scope": "all",
+		"message": {
+			"text": "Jamal Hartnett created a new pull request",
+			"html": "Jamal Hartnett created a new pull request",
+			"markdown": "Jamal Hartnett created a new pull request"
+		},
+		"detailedMessage": {
+			"text": "Jamal Hartnett created a new pull request\r\n\r\n- Merge status: Succeeded\r\n- Merge commit: eef717(https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n",
+			"html": "Jamal Hartnett created a new pull request\r\n<ul>\r\n<li>Merge status: Succeeded</li>\r\n<li>Merge commit: <a href=\"https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72\">eef717</a></li>\r\n</ul>",
+			"markdown": "Jamal Hartnett created a new pull request\r\n\r\n+ Merge status: Succeeded\r\n+ Merge commit: [eef717](https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n"
+		},
+		"resource": {
+			"repository": {
+				"id": "4bc14d40-c903-45e2-872e-0462c7748079",
+				"name": "Fabrikam",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079",
+				"project": {
+					"id": "6ce954b1-ce1f-45d1-b94d-e6bf2464ba2c",
+					"name": "Fabrikam",
+					"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/projects/6ce954b1-ce1f-45d1-b94d-e6bf2464ba2c",
+					"state": "wellFormed"
+				},
+				"defaultBranch": "refs/heads/master",
+				"remoteUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_git/Fabrikam"
+			},
+			"pullRequestId": 1,
+			"status": "active",
+			"createdBy": {
+				"id": "54d125f7-69f7-4191-904f-c5b96b6261c8",
+				"displayName": "Jamal Hartnett",
+				"uniqueName": "fabrikamfiber4@hotmail.com",
+				"url": "https://vssps.dev.azure.com/fabrikam/_apis/Identities/54d125f7-69f7-4191-904f-c5b96b6261c8",
+				"imageUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_api/_common/identityImage?id=54d125f7-69f7-4191-904f-c5b96b6261c8"
+			},
+			"creationDate": "2014-06-17T16:55:46.589889Z",
+			"title": "my first pull request",
+			"description": " - test2\r\n",
+			"sourceRefName": "refs/heads/mytopic",
+			"targetRefName": "refs/heads/master",
+			"mergeStatus": "succeeded",
+			"mergeId": "a10bb228-6ba6-4362-abd7-49ea21333dbd",
+			"lastMergeSourceCommit": {
+				"commitId": "53d54ac915144006c2c9e90d2c7d3880920db49c",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/53d54ac915144006c2c9e90d2c7d3880920db49c"
+			},
+			"lastMergeTargetCommit": {
+				"commitId": "a511f535b1ea495ee0c903badb68fbc83772c882",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/a511f535b1ea495ee0c903badb68fbc83772c882"
+			},
+			"lastMergeCommit": {
+				"commitId": "eef717f69257a6333f221566c1c987dc94cc0d72",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72"
+			},
+			"reviewers": [
+				{
+					"reviewerUrl": null,
+					"vote": 0,
+					"id": "2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"displayName": "[Mobile]\\Mobile Team",
+					"uniqueName": "vstfs:///Classification/TeamProject/f0811a3b-8c8a-4e43-a3bf-9a049b4835bd\\Mobile Team",
+					"url": "https://vssps.dev.azure.com/fabrikam/_apis/Identities/2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"imageUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_api/_common/identityImage?id=2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"isContainer": true
+				}
+			],
+			"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/pullRequests/1"
+		},
+		"resourceVersion": "1.0",
+		"resourceContainers": {
+			"collection": {
+				"id": "c12d0eb8-e382-443b-9f9c-c52cba5014c2"
+			},
+			"account": {
+				"id": "f844ec47-a9db-4511-8281-8b63f4eaf94e"
+			},
+			"project": {
+				"id": "be9b3917-87e6-42a4-a549-2bc06a7a878f"
+			}
+		},
+		"createdDate": "2016-09-19T13:03:27.2879096Z"
+	}`
+
+	sampleCodeGitPullRequestCreatedAndInactive = `{
+		"id": "2ab4e3d3-b7a6-425e-92b1-5a9982c1269e",
+		"eventType": "git.pullrequest.created",
+		"publisherId": "tfs",
+		"scope": "all",
+		"message": {
+			"text": "Jamal Hartnett created a new pull request",
+			"html": "Jamal Hartnett created a new pull request",
+			"markdown": "Jamal Hartnett created a new pull request"
+		},
+		"detailedMessage": {
+			"text": "Jamal Hartnett created a new pull request\r\n\r\n- Merge status: Succeeded\r\n- Merge commit: eef717(https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n",
+			"html": "Jamal Hartnett created a new pull request\r\n<ul>\r\n<li>Merge status: Succeeded</li>\r\n<li>Merge commit: <a href=\"https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72\">eef717</a></li>\r\n</ul>",
+			"markdown": "Jamal Hartnett created a new pull request\r\n\r\n+ Merge status: Succeeded\r\n+ Merge commit: [eef717](https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n"
+		},
+		"resource": {
+			"repository": {
+				"id": "4bc14d40-c903-45e2-872e-0462c7748079",
+				"name": "Fabrikam",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079",
+				"project": {
+					"id": "6ce954b1-ce1f-45d1-b94d-e6bf2464ba2c",
+					"name": "Fabrikam",
+					"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/projects/6ce954b1-ce1f-45d1-b94d-e6bf2464ba2c",
+					"state": "wellFormed"
+				},
+				"defaultBranch": "refs/heads/master",
+				"remoteUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_git/Fabrikam"
+			},
+			"pullRequestId": 1,
+			"status": "completed",
+			"createdBy": {
+				"id": "54d125f7-69f7-4191-904f-c5b96b6261c8",
+				"displayName": "Jamal Hartnett",
+				"uniqueName": "fabrikamfiber4@hotmail.com",
+				"url": "https://vssps.dev.azure.com/fabrikam/_apis/Identities/54d125f7-69f7-4191-904f-c5b96b6261c8",
+				"imageUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_api/_common/identityImage?id=54d125f7-69f7-4191-904f-c5b96b6261c8"
+			},
+			"creationDate": "2014-06-17T16:55:46.589889Z",
+			"title": "my first pull request",
+			"description": " - test2\r\n",
+			"sourceRefName": "refs/heads/mytopic",
+			"targetRefName": "refs/heads/master",
+			"mergeStatus": "succeeded",
+			"mergeId": "a10bb228-6ba6-4362-abd7-49ea21333dbd",
+			"lastMergeSourceCommit": {
+				"commitId": "53d54ac915144006c2c9e90d2c7d3880920db49c",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/53d54ac915144006c2c9e90d2c7d3880920db49c"
+			},
+			"lastMergeTargetCommit": {
+				"commitId": "a511f535b1ea495ee0c903badb68fbc83772c882",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/a511f535b1ea495ee0c903badb68fbc83772c882"
+			},
+			"lastMergeCommit": {
+				"commitId": "eef717f69257a6333f221566c1c987dc94cc0d72",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72"
+			},
+			"reviewers": [
+				{
+					"reviewerUrl": null,
+					"vote": 0,
+					"id": "2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"displayName": "[Mobile]\\Mobile Team",
+					"uniqueName": "vstfs:///Classification/TeamProject/f0811a3b-8c8a-4e43-a3bf-9a049b4835bd\\Mobile Team",
+					"url": "https://vssps.dev.azure.com/fabrikam/_apis/Identities/2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"imageUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_api/_common/identityImage?id=2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"isContainer": true
+				}
+			],
+			"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/pullRequests/1"
+		},
+		"resourceVersion": "1.0",
+		"resourceContainers": {
+			"collection": {
+				"id": "c12d0eb8-e382-443b-9f9c-c52cba5014c2"
+			},
+			"account": {
+				"id": "f844ec47-a9db-4511-8281-8b63f4eaf94e"
+			},
+			"project": {
+				"id": "be9b3917-87e6-42a4-a549-2bc06a7a878f"
+			}
+		},
+		"createdDate": "2016-09-19T13:03:27.2879096Z"
+	}`
+
+	sampleCodeGitPullRequestCreatedMergeFailed = `{
+		"id": "2ab4e3d3-b7a6-425e-92b1-5a9982c1269e",
+		"eventType": "git.pullrequest.created",
+		"publisherId": "tfs",
+		"scope": "all",
+		"message": {
+			"text": "Jamal Hartnett created a new pull request",
+			"html": "Jamal Hartnett created a new pull request",
+			"markdown": "Jamal Hartnett created a new pull request"
+		},
+		"detailedMessage": {
+			"text": "Jamal Hartnett created a new pull request\r\n\r\n- Merge status: Succeeded\r\n- Merge commit: eef717(https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n",
+			"html": "Jamal Hartnett created a new pull request\r\n<ul>\r\n<li>Merge status: Succeeded</li>\r\n<li>Merge commit: <a href=\"https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72\">eef717</a></li>\r\n</ul>",
+			"markdown": "Jamal Hartnett created a new pull request\r\n\r\n+ Merge status: Succeeded\r\n+ Merge commit: [eef717](https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n"
+		},
+		"resource": {
+			"repository": {
+				"id": "4bc14d40-c903-45e2-872e-0462c7748079",
+				"name": "Fabrikam",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079",
+				"project": {
+					"id": "6ce954b1-ce1f-45d1-b94d-e6bf2464ba2c",
+					"name": "Fabrikam",
+					"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/projects/6ce954b1-ce1f-45d1-b94d-e6bf2464ba2c",
+					"state": "wellFormed"
+				},
+				"defaultBranch": "refs/heads/master",
+				"remoteUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_git/Fabrikam"
+			},
+			"pullRequestId": 1,
+			"status": "active",
+			"createdBy": {
+				"id": "54d125f7-69f7-4191-904f-c5b96b6261c8",
+				"displayName": "Jamal Hartnett",
+				"uniqueName": "fabrikamfiber4@hotmail.com",
+				"url": "https://vssps.dev.azure.com/fabrikam/_apis/Identities/54d125f7-69f7-4191-904f-c5b96b6261c8",
+				"imageUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_api/_common/identityImage?id=54d125f7-69f7-4191-904f-c5b96b6261c8"
+			},
+			"creationDate": "2014-06-17T16:55:46.589889Z",
+			"title": "my first pull request",
+			"description": " - test2\r\n",
+			"sourceRefName": "refs/heads/mytopic",
+			"targetRefName": "refs/heads/master",
+			"mergeStatus": "failed",
+			"mergeId": "a10bb228-6ba6-4362-abd7-49ea21333dbd",
+			"lastMergeSourceCommit": {
+				"commitId": "53d54ac915144006c2c9e90d2c7d3880920db49c",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/53d54ac915144006c2c9e90d2c7d3880920db49c"
+			},
+			"lastMergeTargetCommit": {
+				"commitId": "a511f535b1ea495ee0c903badb68fbc83772c882",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/a511f535b1ea495ee0c903badb68fbc83772c882"
+			},
+			"lastMergeCommit": {
+				"commitId": "eef717f69257a6333f221566c1c987dc94cc0d72",
+				"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72"
+			},
+			"reviewers": [
+				{
+					"reviewerUrl": null,
+					"vote": 0,
+					"id": "2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"displayName": "[Mobile]\\Mobile Team",
+					"uniqueName": "vstfs:///Classification/TeamProject/f0811a3b-8c8a-4e43-a3bf-9a049b4835bd\\Mobile Team",
+					"url": "https://vssps.dev.azure.com/fabrikam/_apis/Identities/2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"imageUrl": "https://dev.azure.com/fabrikam/DefaultCollection/_api/_common/identityImage?id=2ea2d095-48f9-4cd6-9966-62f6f574096c",
+					"isContainer": true
+				}
+			],
+			"url": "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/pullRequests/1"
+		},
+		"resourceVersion": "1.0",
+		"resourceContainers": {
+			"collection": {
+				"id": "c12d0eb8-e382-443b-9f9c-c52cba5014c2"
+			},
+			"account": {
+				"id": "f844ec47-a9db-4511-8281-8b63f4eaf94e"
+			},
+			"project": {
+				"id": "be9b3917-87e6-42a4-a549-2bc06a7a878f"
+			}
+		},
+		"createdDate": "2016-09-19T13:03:27.2879096Z"
 	}`
 
 	sampleCodeGitPushWithMultipleCommits = `{
@@ -306,7 +572,7 @@ func Test_HookProvider_TransformRequest(t *testing.T) {
 			Body: ioutil.NopCloser(strings.NewReader(sampleCodeGitPushBadEventType)),
 		}
 		hookTransformResult := provider.TransformRequest(&request)
-		require.EqualError(t, hookTransformResult.Error, "Not a push event, can't start a build")
+		require.EqualError(t, hookTransformResult.Error, "Not a valid event type, skipping")
 		require.False(t, hookTransformResult.ShouldSkip)
 		require.Nil(t, hookTransformResult.TriggerAPIParams)
 		require.Equal(t, false, hookTransformResult.DontWaitForTriggerResponse)
@@ -587,4 +853,60 @@ func Test_HookProvider_TransformRequest(t *testing.T) {
 		}, hookTransformResult.TriggerAPIParams)
 		require.Equal(t, false, hookTransformResult.DontWaitForTriggerResponse)
 	}
+
+	t.Log("Git.pullrequest.created")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"Content-Type": {"application/json; charset=utf-8"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(sampleCodeGitPullRequestCreated)),
+		}
+		pullRequestID := 1
+		hookTransformResult := provider.TransformRequest(&request)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, []bitriseapi.TriggerAPIParamsModel{
+			{
+				BuildParams: bitriseapi.BuildParamsModel{
+					CommitHash:               "eef717f69257a6333f221566c1c987dc94cc0d72",
+					CommitMessage:            "Jamal Hartnett created a new pull request\r\n\r\n- Merge status: Succeeded\r\n- Merge commit: eef717(https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079/commits/eef717f69257a6333f221566c1c987dc94cc0d72)\r\n",
+					Branch:                   "mytopic",
+					BranchDest:               "master",
+					PullRequestID:            &pullRequestID,
+					PullRequestRepositoryURL: "https://dev.azure.com/fabrikam/DefaultCollection/_apis/repos/git/repositories/4bc14d40-c903-45e2-872e-0462c7748079",
+				},
+			},
+		}, hookTransformResult.TriggerAPIParams)
+		require.Equal(t, false, hookTransformResult.DontWaitForTriggerResponse)
+	}
+
+	t.Log("Git.pullrequest.created - Already completed pull request")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"Content-Type": {"application/json; charset=utf-8"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(sampleCodeGitPullRequestCreatedAndInactive)),
+		}
+		hookTransformResult := provider.TransformRequest(&request)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Pull request created, and completed - does not require a build")
+		require.Equal(t, false, hookTransformResult.DontWaitForTriggerResponse)
+	}
+
+	t.Log("Git.pullrequest.created - Merge failed")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"Content-Type": {"application/json; charset=utf-8"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(sampleCodeGitPullRequestCreatedMergeFailed)),
+		}
+		hookTransformResult := provider.TransformRequest(&request)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Pull request created but merge failed - not building")
+		require.Equal(t, false, hookTransformResult.DontWaitForTriggerResponse)
+	}
+
 }
